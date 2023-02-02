233fceca7af47d51af05cd059545a9cb448f52e2
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-02-02 16:20:04 -0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -14,4 +14,3 @@ export function App() {
     </div>
   )
 }
-
