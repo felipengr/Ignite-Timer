b4c05e16fd08d3b94c7d50c24c2de51d42a5cbf6
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-02-02 15:42:37 -0300

    aprendendo styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { Button } from "./components/Button"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
     <div className="App">
-      
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+      </ThemeProvider>
     </div>
   )
 }
