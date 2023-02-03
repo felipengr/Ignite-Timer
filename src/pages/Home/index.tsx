691d5e7559879ab33649474a8830f10d24bb1572
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-03 15:44:39 -0300

    TypeScript no formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,12 +20,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFomeData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch, formState } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFomeData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFomeData) {
     console.log(data)
   }
 
