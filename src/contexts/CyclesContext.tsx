10a8c132b4b156b6e48b04f1e77c9d376616ff3a
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-06 20:08:52 -0300

    Reset do formulário

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -69,8 +69,6 @@ export function CyclesContextProvider({
     setCyles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
