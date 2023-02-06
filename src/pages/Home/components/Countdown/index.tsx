a3afab772490fbe3e0e595c62b507fce77f02337
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-06 17:30:29 -0300

    fix return in countdown

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,10 +1,11 @@
-import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 import { CyclesContext } from '../..'
 import { CountdownContainer, Separator } from './styles'
 
 export function Countdown() {
-  const {activeCycle, activeCycleId, markCurrentCycleAsFinished} = useContext(CyclesContext)
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
+    useContext(CyclesContext)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -48,11 +49,13 @@ export function Countdown() {
     }
   }, [minutes, seconds, activeCycle])
 
-  <CountdownContainer>
-    <span>{minutes[0]}</span>
-    <span>{minutes[1]}</span>
-    <Separator>:</Separator>
-    <span>{seconds[0]}</span>
-    <span>{seconds[1]}</span>
-  </CountdownContainer>
+  return (
+    <CountdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownContainer>
+  )
 }
